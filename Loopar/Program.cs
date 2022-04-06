ec8cd69fabfdffad7fef515c4e50b5a6c0464aa1
AxelRysjöLisslö <rysjolisslo.axel@elev.ga.ntig.se>
2022-04-06 13:46:46 +0200

    commit

--- Loopar/Program.cs
+++ Loopar/Program.cs
@@ -1,2 +1,25 @@
 ﻿using System;
 
+ 
+Console.WriteLine("gissa siffran! den ska vara mer än 3 men mindre än 9.");
+bool fråga =false;
+int i =10;
+
+while (fråga==false)
+{
+    Console.WriteLine(i);
+    if (i=5)
+    {
+        Console.WriteLine("Nice");
+       
+    }
+    Console.ReadLine();
+    
+}
+
+
+
+
+
+
+
